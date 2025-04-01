d3d190941420c6ee60fc34df0eaf7801929dfc91
Taylor Hancock <58285541+SteelStarling@users.noreply.github.com>
2025-04-01 00:57:34 -0700

    Initial commit

---
+++ error_analysis.py
@@ -0,0 +1,26 @@
+"""Error analysis software for supersonic and hypersonic equations
+Author: Taylor Hancock
+Date:   03/31/2025
+Class:  AE302 - Aerodynamics 2
+Assignment: Project 02 - Error Analysis
+"""
+
+def plot_absolute_error_pressure():
+    """Plot absolute error in the pressure equations"""
+    pass
+
+def plot_relative_error_pressure():
+    """Plot the region where the relative error in pressure is less than 0.1"""
+    pass
+
+def plot_absolute_error_density():
+    """Plot absolute error in the density equations"""
+    pass
+
+def plot_relative_error_density():
+    """Plot the region where the relative error in density is less than 0.1"""
+    pass
+
+def plot_relative_error_intersection():
+    """Plot the region where the relative error of pressure and density are both < 0.1"""
+    pass
